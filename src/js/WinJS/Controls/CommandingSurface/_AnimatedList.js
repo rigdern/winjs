6271980e1c5ba50fed5b34d4b97c365f8fe3650b
Adam Comella <rigdern@gmail.com>
2015-08-16 16:51:39 -0700

    Update AnimatedList to use new interface

--- src/js/WinJS/Controls/CommandingSurface/_AnimatedList.js
+++ src/js/WinJS/Controls/CommandingSurface/_AnimatedList.js
@@ -1,5 +1,10 @@
 define([
 ], function () {
+    var ClassNames = {
+        animatedList: "win-animatedlist",
+        removing: "win-animatedlist-removing"
+    };
+    
     // Returns elements of `a` that aren't in `b`
     function arrayDiff(a, b) {
         return a.filter(function (aEl) {
@@ -8,25 +13,72 @@ define([
     }
     
     var AnimatedList = WinJS.Class.define(function (element, options) {
-        element.style.position = "relative";
-        this._dom = {
-            root: element
-        };
+        this._initializeDom(element || document.createElement("div"));
         this._rendered = {
             items: []
         };
         this._items = [];
+        this._animationPromise = null;
         this._animating = false;
         this._queued = false;
     }, {
-        setItems: function (items, options) {
-            options = options || {};
-            var skipAnimations = options.skipAnimations;
-            this._items = items.slice(0); // defensive copy
-            this._updateDom();
+        children: {
+            get: function () {
+                return this._items;
+            },
+            set: function (value) {
+                this._items = value.slice(0);
+                this._updateDom();
+            }
+        },
+        childernInDom: {
+            get: function () {
+                return this._rendered.items;
+            },
+            set: function (value) {
+                this._items = value.slice(0);
+                this._updateDomWithoutAnimations();
+            }
+        },
+        appendChild: function (element) {
+            this.removeChild(element);
+            this._rendered.items.push(element);
+            this._dom.root.appendChild(element);
+        },
+        removeChild: function (element) {
+            var index = this._rendered.items.indexOf(element);
+            if (index !== -1) {
+                this._rendered.items.splice(index, 1);
+                this._dom.root.removeChild(element);
+            }
+        },
+        _initializeDom: function (root) {
+            root.classList.add(ClassNames.animatedList);
+            
+            this._dom = {
+                root: root
+            };
+        },
+        _updateDomWithoutAnimations: function () {
+            if (this._animationPromise) {
+                this._queued = false;
+                this._animationPromise.cancel();
+            }
+            
+            var rendered = this._rendered;
+            
+            var prevItems = rendered.items;
+            var nextItems = this._items;
+            
+            prevItems.forEach(function (e) {
+                e.parentNode && e.parentNode.removeChild(e);
+            });
+            nextItems.forEach(function (e) {
+                this._dom.root.appendChild(e);
+            }, this);
         },
         _updateDom: function () {
-            if (this._animating) {
+            if (this._animationPromise) {
                 this._queued = true;
                 return;
             }
@@ -34,10 +86,10 @@ define([
             var rendered = this._rendered;
             
             var prevItems = rendered.items;
-            var newItems = this._items;
+            var nextItems = this._items;
             
-            var added = arrayDiff(newItems, prevItems);
-            var removed = arrayDiff(prevItems, newItems);
+            var added = arrayDiff(nextItems, prevItems);
+            var removed = arrayDiff(prevItems, nextItems);
             var affected = arrayDiff(prevItems, removed);
             
             var animation = WinJS.UI.Animation._createUpdateListAnimation(added, removed, affected);
@@ -45,23 +97,22 @@ define([
             prevItems.forEach(function (e) {
                 e.parentNode && e.parentNode.removeChild(e);
             });
-            newItems.forEach(function (e) {
+            nextItems.forEach(function (e) {
                 this._dom.root.appendChild(e);
             }, this);
             removed.forEach(function (e) {
-                e.classList.add("win-removing");
+                e.classList.add(ClassNames.removing);
                 this._dom.root.appendChild(e);
             }, this);
             
-            rendered.items = newItems;
+            rendered.items = nextItems;
             
-            this._animating = true;
-            animation.execute().then(function () {
+            this._animationPromise = animation.execute().then(function () {
                 removed.forEach(function (e) {
-                    e.classList.remove("win-removing");
+                    e.classList.remove(ClassNames.removing);
                     e.parentNode && e.parentNode.removeChild(e);
                 });
-                this._animating = false;
+                this._animationPromise = null;
                 if (this._queued) {
                     this._queued = false;
                     this._updateDom();
